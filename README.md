ef41b42b123eeb1a7aabba25ceae6520eb6721b8
abbform <abbform@gmail.com>
2018-12-25 08:50:07 +0800

    first commmit

---
+++ README.md
@@ -0,0 +1,5 @@
+熟悉一下rust的用法，正好也没有刷过leetcode
+
+抄了很多来自于 https://github.com/u2/leetcode-rust 的东西
+
+需要nightly才能正常运行
---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#![feature(test)]
+extern crate test;
+
+pub mod two_sum;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
