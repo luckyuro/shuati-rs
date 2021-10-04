2831bae99c420ed4ed539ac441c83095bdf7d01f
abbform <abbform@gmail.com>
2021-10-04 21:46:47 +0800

    chore: rename leetcode problem name

--- src/leetcode.rs
+++ src/leetcode.rs
@@ -1,35 +1,35 @@
 struct Solution {}
 
-pub mod p1_two_sum;
-pub mod p2_add_two_numbers;
-pub mod p3_length_of_longest_substring;
-pub mod p4_find_median_sorted_arrays;
-pub mod p5_longest_palindrome;
-pub mod p6_convert;
-pub mod p7_reverse_interger;
-pub mod p8_my_atoi;
-pub mod p9_is_palindrome;
+pub mod p0001_two_sum;
+pub mod p0002_add_two_numbers;
+pub mod p0003_length_of_longest_substring;
+pub mod p0004_find_median_sorted_arrays;
+pub mod p0005_longest_palindrome;
+pub mod p0006_convert;
+pub mod p0007_reverse_interger;
+pub mod p0008_my_atoi;
+pub mod p0009_is_palindrome;
 
-pub mod p11_max_area;
+pub mod p0011_max_area;
 
-pub mod p84_largest_rectangle_area;
-pub mod p85_maximal_rectangle;
+pub mod p0084_largest_rectangle_area;
+pub mod p0085_maximal_rectangle;
 
-pub mod p134_gas_station;
-pub mod p136_single_number;
-pub mod p137_single_number;
+pub mod p0134_gas_station;
+pub mod p0136_single_number;
+pub mod p0137_single_number;
 
-pub mod p543_diameter_of_binary_tree;
+pub mod p0543_diameter_of_binary_tree;
 
-pub mod p743_network_delay_time;
-pub mod p915_partition_disjoint;
+pub mod p0743_network_delay_time;
+pub mod p0915_partition_disjoint;
 
-pub mod p922_sort_array_by_parity_ii;
-pub mod p923_three_sum_multi;
+pub mod p0922_sort_array_by_parity_ii;
+pub mod p0923_three_sum_multi;
 
-pub mod p925_is_long_pressed_name;
+pub mod p0925_is_long_pressed_name;
 
-pub mod p983_mincost_tickets;
+pub mod p0983_mincost_tickets;
 
 pub mod p1111_max_depth_after_split;
 pub mod p1190_reverse_parentheses;
