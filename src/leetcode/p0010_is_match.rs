9fbbb1b7966ef663ab0c2da4fac130dd91c383ff
JJ.Shi <abbform@gmail.com>
2023-07-31 02:23:53 +0800

    finish leetcode p10

--- src/leetcode/p0010_is_match.rs
+++ src/leetcode/p0010_is_match.rs
@@ -10,26 +10,38 @@ impl Solution {
         const STAR: u8 = '*' as u8;
         const DOT: u8 = '.' as u8;
 
-        for (pi, pat) in p.iter().enumerate() {
-            if pat == &STAR {
-                for (si, c) in s.iter().enumerate() {
-                    if si == 0 {
-                        continue;
-                    }
-                    let pat = &p[pi - 1];
-                    if pat == c || pat == &DOT {
-                        m[pi + 1][si + 1] = m[pi][si];
+        for i in (2..=p.len()).step_by(2) {
+            if p[i - 1] == STAR {
+                m[i][0] = true;
+            } else {
+                break;
+            }
+        }
+
+        for mpi in 1..=p.len() {
+            let pat = p[mpi - 1];
+            if pat == STAR {
+                let rel_pat = p[mpi - 2];
+                for msi in 1..=s.len() {
+                    let c = s[msi - 1];
+                    if rel_pat == c || rel_pat == DOT {
+                        // situation:
+                        // 1. -delete- match for multiple times  m[mpi - 1][msi - 1] not needed, covered by case 3
+                        // 2. already matched once and only once
+                        // 3. current line has matched and continue match
+                        // 4. even matched but count zero (same pattern as former)
+                        m[mpi][msi] = m[mpi - 1][msi] || m[mpi][msi - 1] || m[mpi - 2][msi];
                     } else {
-                        m[pi + 1][si + 1] = m[pi][si + 1]
-                            || m[pi + 1][si]
-                            || m[pi - 1][si + 1]
-                            || m[pi - 1][si - 1];
+                        // situation:
+                        // 1. the count is zero
+                        m[mpi][msi] = m[mpi - 2][msi];
                     }
                 }
-                continue;
-            }
-            for (si, c) in s.iter().enumerate() {
-                m[pi + 1][si + 1] = m[pi][si] && (pat == c || pat == &DOT);
+            } else {
+                for msi in 1..=s.len() {
+                    let c = s[msi - 1];
+                    m[mpi][msi] = m[mpi - 1][msi - 1] && (pat == c || pat == DOT);
+                }
             }
         }
         print!("{:?}", &m);
@@ -40,9 +52,13 @@ mod test {
     use super::Solution;
     #[test]
     fn test_is_match() {
-        // assert_eq!(Solution::is_match("aa".to_string(), "a".to_string()), false);
-        // assert_eq!(Solution::is_match("aa".to_string(), "a*".to_string()), true);
-        //  assert_eq!(Solution::is_match("ab".to_string(), ".*".to_string()), true);
+        assert_eq!(Solution::is_match("aa".to_string(), "a".to_string()), false);
+        assert_eq!(Solution::is_match("aa".to_string(), "a*".to_string()), true);
+        assert_eq!(Solution::is_match("ab".to_string(), ".*".to_string()), true);
+        assert_eq!(
+            Solution::is_match("aaa".to_string(), ".*".to_string()),
+            true
+        );
         assert_eq!(
             Solution::is_match("aab".to_string(), "c*a*b*".to_string()),
             true
